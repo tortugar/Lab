a2d2f2a19c086db3580ca5b4e3fc8935ff081144
tortugar <tortugar@gingam-obba.local>
2019-06-21 18:59:04 -0400

    fixed bug in call of tdt.read_block

--- TDTFear/extract_videoframes.py
+++ TDTFear/extract_videoframes.py
@@ -5,64 +5,68 @@ Created on Thu May 24 11:50:26 2018
 @author: ChungWeberPC_04
 """
 # extract movie frames
-
 import subprocess
 from shutil import make_archive, rmtree
 import os
 import re
 import Tkinter as Tk
 import tkFileDialog as tkf
+import tdt
+import scipy.io as so
 
-
-flashdir = 'C:\Users\ChungWeberPC_04\Documents\Tank'
+flashdir = 'C:/Users/ChungWeberPC_04/Documents/Tank'
 root = Tk.Tk()
 tank_dir = tkf.askdirectory(initialdir = flashdir)
 root.update()
 
-params = {}
+params = dict()
 params['tank'] = tank_dir
 
-
-
-print "trying to get timing for video..."
+# get video timing
+print("trying to get timing for video...")
 # Extract video timing information
-tank = '\\'.join(re.split('\\\\', params['tank'])[0:-1])
-block = re.split('\\\\', params['tank'])[-1]
-import win32com.client
-h = win32com.client.Dispatch('matlab.application')
-cmd  =  "data = TDTbin2mat(fullfile('%s' ,'%s'),'Type',{'epocs'}); \
-        a = fields(data.epocs); cam = a{1}; \
-        onset = eval(['data.epocs.' cam '.onset']); \
-        offset= eval(['data.epocs.' cam '.offset']); \
-        save(fullfile('%s', '%s','video_timing.mat'), 'onset', 'offset')" % (tank,block, tank,block)
-print cmd
+# tank = '\\'.join(re.split('\\\\', params['tank'])[0:-1])
+# block = re.split('\\\\', params['tank'])[-1]
+# import win32com.client
+# h = win32com.client.Dispatch('matlab.application')
+# cmd  =  "data = TDTbin2mat(fullfile('%s' ,'%s'),'Type',{'epocs'}); \
+#         a = fields(data.epocs); cam = a{1}; \
+#         onset = eval(['data.epocs.' cam '.onset']); \
+#         offset= eval(['data.epocs.' cam '.offset']); \
+#         save(fullfile('%s', '%s','video_timing.mat'), 'onset', 'offset')" % (tank,block, tank,block)
+# print(cmd)
+# while not os.path.isfile(os.path.join(params['tank'], 'video_timing.mat')):
+#     print('...didnt work, trying again...')
+#     try:
+#         h.Execute(cmd)
+#     except:
+#         pass
+# print("...done")
+# h.quit()
+tank = params['tank']
+ep_data = tdt.read_block(tank, evtype=['epocs'])
+onset = ep_data.epocs.Cam1.onset
+offset = ep_data.epocs.Cam1.offset
+so.savemat(os.path.join(tank, 'video_timing.mat'), {'onset': onset, 'offset': offset})
 
-while not os.path.isfile(os.path.join(params['tank'], 'video_timing.mat')):
-    print '...didnt work, trying again...'
-    try:
-        h.Execute(cmd)
-    except:
-        pass
-print "...done"
-h.quit()
 
+# extracting single movide frames
 ffmpeg_path = 'C:\\Users\ChungWeberPC_04\\Documents\\Ground\\ffmpeg-20180227-fa0c9d6-win64-static\\bin\\ffmpeg'
 video_file = [f for f in os.listdir(params['tank']) if re.match('^.*\.avi$', f)][0]
 if not(os.path.isdir(os.path.join(params['tank'], 'Stack'))):
     os.mkdir(os.path.join(params['tank'], 'Stack'))
-    
 
-print "extracting single movie frames..."
+print("extracting single movie frames...")
 cwd = os.getcwd()
 os.chdir(params['tank'])
 s = ffmpeg_path + ' -i ' + video_file + ' Stack\\fig%d.jpg'
 subprocess.call(s, stdout=subprocess.PIPE, shell=True)
 
-
-print "zipping movie frames..."
+# zipping movie frames into zip stack
+print("zipping movie frames...")
 make_archive('stack', 'zip', params['tank'], base_dir='Stack')
 if os.path.isdir('Stack'):
     rmtree('Stack')
 os.chdir(cwd)
-print "Everything seems to have worked fine..."   
+print("Everything seems to have worked fine...")
 
